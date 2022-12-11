d36aebc63102ddd910e0a5311feff351a5dcb158
Eri JS <erijohnsons.fernandes@gmail.com>
2022-12-11 17:38:16 -0300

    refactor: remove unnecessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MembersModule } from './members/members.module';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
+import { AuthModule } from './auth/auth.module';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { MembersModule } from './members/members.module';
 import { RolesGuard } from './roles/roles.guard';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [MembersModule, UsersModule, AuthModule],
-  controllers: [AppController],
+  controllers: [],
   providers: [
-    AppService,
     {
       provide: APP_GUARD,
       useClass: JwtAuthGuard,
